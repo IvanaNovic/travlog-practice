61c8d0b90f1507ab22324c0097a88bfbc36215f4
IvanaKrehovski <ikrehovski@gmail.com>
2024-07-17 11:41:21 +0200

    'wip'

--- src/Components/pages/Home.tsx
+++ src/Components/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './hero/Hero';
 import SponsorSlider from './sponsorsSlider/SponsorSlider';
+import Services from './sectionServices/Services';
 
 const Home = () => {
     return (
         <>
             <Hero />
             <SponsorSlider />
+            <Services />
         </>
     );
 };
