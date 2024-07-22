5bd6ef16cfb3209c5c3f93a85cc4a6fa6554ed43
IvanaKrehovski <ikrehovski@gmail.com>
2024-07-22 12:18:47 +0200

    wip

--- src/Components/pages/topDestination/DestinationSlider.tsx
+++ src/Components/pages/topDestination/DestinationSlider.tsx
@@ -16,6 +16,15 @@ type SliderRef = Slider & {
     slickNext: () => void;
 };
 
+interface Slide {
+    id: number;
+    image: string;
+    title: string;
+    price: string;
+    destination: string;
+    rating: string;
+}
+
 function DestinationSlider() {
     const sliderRef = useRef<SliderRef>(null);
 
@@ -31,6 +40,35 @@ function DestinationSlider() {
         }
     };
 
+    const createSlide = (slide: Slide) => (
+        <div key={slide.id} className="w-full p-4">
+            <div className="flex h-[500px] flex-col rounded-lg bg-white shadow-lg md:h-[575px]">
+                <img
+                    src={slide.image}
+                    alt={slide.title}
+                    className="mx-auto h-full rounded-t-lg object-cover"
+                />
+                <div className="flex h-full flex-col gap-8 p-4 md:justify-evenly lg:p-6">
+                    <div className="flex flex-col md:gap-7 lg:flex-row-reverse">
+                        <h2 className="text-start text-lg font-bold text-secondary lg:text-end">
+                            {slide.price}
+                        </h2>
+                        <h2 className="text-start text-xl font-bold">
+                            {slide.title}
+                        </h2>
+                    </div>
+                    <p className="text-left text-lg text-neutral">
+                        {slide.destination}
+                    </p>
+                    <div className="flex flex-row gap-2">
+                        <p className="font-bold text-orange">{slide.rating}</p>
+                        <img src={ratingStar} alt="Rating star" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
     return (
         <div>
             <div className="hidden w-full py-8 md:block">
@@ -38,48 +76,16 @@ function DestinationSlider() {
                     <button
                         onClick={previous}
                         className="shadow-customShadow flex h-16 w-16 rotate-180 items-center justify-center rounded-full bg-primary">
-                        <img src={arrow} alt="Arrow left" className="" />
+                        <img src={arrow} alt="Arrow left" />
                     </button>
                     <button
                         onClick={next}
                         className="flex h-16 w-16 items-center justify-center rounded-full bg-primary shadow-buttonShadow">
-                        <img src={arrow} alt="Arrow left" className="" />
+                        <img src={arrow} alt="Arrow right" />
                     </button>
                 </div>
                 <Slider ref={sliderRef} {...settingsDestinationSlide}>
-                    {slidesDestinations.map((slide) => (
-                        <div key={slide.id} className="w-full p-4">
-                            <div className="flex h-[575px] flex-col rounded-lg bg-white shadow-lg">
-                                <img
-                                    src={slide.image}
-                                    alt={slide.title}
-                                    className="mx-auto h-full rounded-t-lg object-cover"
-                                />
-                                <div className="flex h-full flex-col gap-16 p-4 xl:p-8">
-                                    <div className="flex flex-col lg:flex-row-reverse lg:gap-6">
-                                        <h2 className="text-start text-lg font-bold text-secondary lg:text-end">
-                                            {slide.price}
-                                        </h2>
-                                        <h2 className="text-start text-xl font-bold">
-                                            {slide.title}
-                                        </h2>
-                                    </div>
-                                    <p className="text-left text-lg text-neutral">
-                                        {slide.destination}
-                                    </p>
-                                    <div className="flex flex-row gap-2">
-                                        <p className="font-bold text-orange">
-                                            {slide.rating}
-                                        </p>
-                                        <img
-                                            src={ratingStar}
-                                            alt="Rating star"
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+                    {slidesDestinations.map(createSlide)}
                 </Slider>
             </div>
             <div className="block md:hidden">
@@ -87,48 +93,16 @@ function DestinationSlider() {
                     <button
                         onClick={previous}
                         className="shadow-customShadow flex h-12 w-12 rotate-180 items-center justify-center rounded-full bg-primary">
-                        <img src={arrow} alt="Arrow left" className="" />
+                        <img src={arrow} alt="Arrow left" />
                     </button>
                     <button
                         onClick={next}
                         className="flex h-12 w-12 items-center justify-center rounded-full bg-primary shadow-buttonShadow">
-                        <img src={arrow} alt="Arrow left" className="" />
+                        <img src={arrow} alt="Arrow right" />
                     </button>
                 </div>
                 <Slider ref={sliderRef} {...settingsMobileDestinationSlide}>
-                    {slidesDestinations.map((slide) => (
-                        <div key={slide.id} className="w-full p-4">
-                            <div className="flex flex-col rounded-lg bg-white pb-0 shadow-lg">
-                                <img
-                                    src={slide.image}
-                                    alt={slide.title}
-                                    className="mx-auto h-full rounded-t-lg object-cover"
-                                />
-                                <div className="flex h-full flex-col gap-8 p-4">
-                                    <div className="flex flex-col">
-                                        <h2 className="text-start text-lg font-bold text-secondary lg:text-end">
-                                            {slide.price}
-                                        </h2>
-                                        <h2 className="text-start text-xl font-bold">
-                                            {slide.title}
-                                        </h2>
-                                    </div>
-                                    <p className="text-left text-lg text-neutral">
-                                        {slide.destination}
-                                    </p>
-                                    <div className="flex flex-row gap-2">
-                                        <p className="font-bold text-orange">
-                                            {slide.rating}
-                                        </p>
-                                        <img
-                                            src={ratingStar}
-                                            alt="Rating star"
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+                    {slidesDestinations.map(createSlide)}
                 </Slider>
             </div>
         </div>
--- src/Components/pages/topDestination/TopDestination.tsx
+++ src/Components/pages/topDestination/TopDestination.tsx
@@ -4,10 +4,12 @@ function TopDestination() {
     return (
         <section className="overflow-hidden">
             <div className="text-center lg:translate-y-24 lg:pl-4 lg:text-start">
-                <p className="text-2xl font-bold text-secondary">
+                <p className="text-base font-bold text-secondary md:text-2xl">
                     TOP DESTINATION
                 </p>
-                <p className="text-4xl font-bold">Explore top destination</p>
+                <p className="text-2xl font-bold md:text-4xl">
+                    Explore top destination
+                </p>
             </div>
             <div>
                 <DestinationSlider />
